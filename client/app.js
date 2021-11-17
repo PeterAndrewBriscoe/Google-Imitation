980afc062670fbd66f1e546df6ba091cf2de6274
Peter Andrews-Briscoe <63236951+PeterAndrewBriscoe@users.noreply.github.com>
2021-11-17 13:30:16 +0000

    sorted out hiding homepage

--- client/app.js
+++ client/app.js
@@ -1,18 +1,22 @@
 const form = document.querySelector('form')
 
-form.addEventListener('#google', getResults)
+form.addEventListener('submit', getResults)
 form.addEventListener('#lucky', getOneResult)
 
 function getResults(e) {
     e.preventDefault()
-    fetch(`http://localhost:3000/${input}`)
-      .then(resp => resp.text())
-      .then(quote => document.getElementById('quote').textContent = quote)
+    document.querySelector('.homepage').style.display = 'none'
+    console.log('I\'m here')
+
+    // fetch(`http://localhost:3000/resultsList`)
+    //   .then(resp => resp.text())
+    //   .then(result => document.getElementById('results').textContent = result)
+    //   .then(document.getElementsByClassName('homepage').style.display = 'none')
 }
 
 function getOneResult(e) {
     e.preventDefault()
-    fetch(`http://localhost:3000/${input}`)
+    fetch(`http://localhost:3000/results/:index`)
       .then(resp => resp.text())
       .then(quote => document.getElementById('quote').textContent = quote)
 }

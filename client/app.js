b5bcdda2dff911e8c5e68b9b9d0b3a4b6046ff3e
Peter Andrews-Briscoe <63236951+PeterAndrewBriscoe@users.noreply.github.com>
2021-11-17 12:10:27 +0000

    put in some basic javascript

--- client/app.js
+++ client/app.js
@@ -1,4 +1,18 @@
 const form = document.querySelector('form')
 
-form.addEventListener('#google', getRandomQuote)
-form.addEventListener('#lucky', getRandomQuote)
+form.addEventListener('#google', getResults)
+form.addEventListener('#lucky', getOneResult)
+
+function getResults(e) {
+    e.preventDefault()
+    fetch(`http://localhost:3000/${input}`)
+      .then(resp => resp.text())
+      .then(quote => document.getElementById('quote').textContent = quote)
+}
+
+function getOneResult(e) {
+    e.preventDefault()
+    fetch(`http://localhost:3000/${input}`)
+      .then(resp => resp.text())
+      .then(quote => document.getElementById('quote').textContent = quote)
+}
